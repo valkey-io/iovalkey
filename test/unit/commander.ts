2f819539f3718967f2d63793ce560ed135543923
jhpung <jhpung.dev@gmail.com>
2024-10-19 02:35:52 +0900

    test: add zscan tests
    Signed-off-by: jhpung <jhpung.dev@gmail.com>

--- test/unit/commander.ts
+++ test/unit/commander.ts
@@ -1,6 +1,7 @@
 import * as sinon from "sinon";
 import { expect } from "chai";
 import Commander from "../../lib/utils/Commander";
+import Command from "../../lib/Command";
 
 describe("Commander", () => {
   describe("#getBuiltinCommands()", () => {
@@ -62,5 +63,21 @@ describe("Commander", () => {
     expect(command.args.length).to.eql(2);
 
     Commander.prototype.sendCommand.restore();
+  });
+
+  describe("#zscan", () => {
+    it("should pass noscores option", async (done) => {
+      const args: any[] = ["key", "0", "MATCH", "pattern", "COUNT", "10", "noscores"];
+      sinon.stub(Commander.prototype, "sendCommand").callsFake((command) => {
+        if(command.args.every((arg, index) => arg === args[index])) {
+          return done();
+        }
+        return done(new Error(`args should be ${args.join(", ")}`));
+      });
+      const c = new Commander();
+
+      await c.zscan(args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
+      (Commander.prototype.sendCommand as any).restore();
+    });
   });
 });
