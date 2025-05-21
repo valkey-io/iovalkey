27bcab5c3a24ec6f88fcad5e7a441cd3ce67f2ab
Martin Slota <martin.slota@workday.com>
2025-05-21 12:08:26 +0200

    Add a regression test
    Signed-off-by: Martin Slota <martin.slota@workday.com>

---
+++ test/functional/cluster/ConnectionPool.ts
@@ -0,0 +1,11 @@
+import { expect } from "chai";
+
+import ConnectionPool from "../../../lib/cluster/ConnectionPool";
+
+describe("The cluster connection pool", () => {
+  describe("when not connected", () => {
+    it("does not throw when fetching a sample node", () => {
+      expect(new ConnectionPool({}).getSampleInstance("all")).to.be.undefined;
+    });
+  });
+});
